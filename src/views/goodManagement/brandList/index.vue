77c9c779607e8c33fd6a4bc19bf56f0257fa152c
LFN <124547864@qq.com>
2018-10-09 00:45:24 +0800

    品牌列表-终止合作弹层界面完成

--- src/views/goodManagement/brandList/index.vue
+++ src/views/goodManagement/brandList/index.vue
@@ -59,7 +59,7 @@
           </el-button>
           <el-button
             size="mini"
-            @click="">终止合作
+            @click="stopCooperationShow(scope.$index, scope.row)">终止合作
           </el-button>
         </template>
       </el-table-column>
@@ -81,6 +81,39 @@
     <el-dialog :visible.sync="isEditBrandShow" width="75%" @close="isEditBrandShow = false" title="编辑品牌">
       <editBrand :brandObj="currentBrand"></editBrand>
     </el-dialog>
+    <el-dialog :visible.sync="isStopCooperationShow" class="" width="70%" @close="isStopCooperationShow = false" title="终止合作">
+      <el-form :model="stopCooperation_ruleForm" :rules="stopCooperation_rules" ref="stopCooperation_ruleForm" label-width="150px" class="demo-ruleForm">
+        <el-form-item label="申请中止时间">
+          <el-date-picker
+            v-model="stopCooperation_ruleForm.applyTime"
+            type="date"
+            placeholder="选择日期">
+          </el-date-picker>
+        </el-form-item>
+        <el-form-item label="申请终止类型" prop="operateType">
+          <el-radio-group v-model="stopCooperation_ruleForm.operateType">
+            <el-radio label="提前终止"></el-radio>
+            <el-radio label="到期终止"></el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="申请理由" prop="applyReason">
+          <el-input type="textarea" placeholder="请输入申请理由" v-model="stopCooperation_ruleForm.applyReason"></el-input>
+        </el-form-item>
+        <el-form-item label="品牌状态">
+          <el-select v-model="stopCooperation_ruleForm.brandStatus" placeholder="请选择">
+            <el-option
+              v-for="item in brandStatusOptions"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitDelChannelForm('deleteChannel_ruleForm')">保存并提交</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 
@@ -100,7 +133,22 @@
         },
         isAddBrandShow: false,
         isEditBrandShow: false,
-        currentBrand: {}
+        currentBrand: {},
+        isStopCooperationShow: false,
+        stopCooperation_ruleForm: {
+          applyReason: null,
+          operateType: null,
+          applyTime: null,
+          brandStatus: ''
+        },
+        stopCooperation_rules: {},
+        brandStatusOptions: [{
+          value: '1',
+          label: '正常供货'
+        },{
+          value: '2',
+          label: '停止供货'
+        }],
       }
     },
     methods: {
@@ -125,6 +173,9 @@
       showEditBrand(row) {
         this.isEditBrandShow = true
         this.currentBrand = row
+      },
+      stopCooperationShow() {
+        this.isStopCooperationShow = true
       }
     },
     mounted() {
