ac316b8fcf4d41fc73d258da948fa4b78bc13e1e
LFN <124547864@qq.com>
2018-11-23 01:02:14 +0800

    普通提交

--- src/views/channelManagement/newManagement/newSubChannel/toConfirm.vue
+++ src/views/channelManagement/newManagement/newSubChannel/toConfirm.vue
@@ -3,79 +3,83 @@
     <!--基本信息below-->
     <h3 class="form-part-title">基本信息</h3>
     <!--content-->
-    <el-row style="border-top:solid 1px #D5D5D5">
-      <el-col :span="5">
-        <div class="grid-content bg-purple ">{{'经营主体'}}</div>
-      </el-col>
-      <el-col :span="19">
-        <div class="grid-content bg-purple-light " @click="">{{'企业'}}</div>
-      </el-col>
-    </el-row>
-    <el-row>
-      <el-col :span="5">
-        <div class="grid-content bg-purple ">{{'公司名称'}}</div>
-      </el-col>
-      <el-col :span="19">
-        <div class="grid-content bg-purple-light " @click="">{{'QWERTY公司'}}</div>
-      </el-col>
-    </el-row>
-    <el-row>
-      <el-col :span="5">
-        <div class="grid-content bg-purple ">{{'营业执照'}}</div>
-      </el-col>
-      <el-col :span="19">
-        <div class="grid-content bg-purple-light " @click="">{{'12345678'}}</div>
-      </el-col>
-    </el-row>
+    <div class="border1">
+      <el-row style="border-top:solid 1px #D5D5D5">
+        <el-col :span="5">
+          <div class="grid-content bg-purple ">{{'经营主体'}}</div>
+        </el-col>
+        <el-col :span="19">
+          <div class="grid-content bg-purple-light " @click="">{{'企业'}}</div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="5">
+          <div class="grid-content bg-purple ">{{'公司名称'}}</div>
+        </el-col>
+        <el-col :span="19">
+          <div class="grid-content bg-purple-light " @click="">{{'QWERTY公司'}}</div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="5">
+          <div class="grid-content bg-purple ">{{'营业执照'}}</div>
+        </el-col>
+        <el-col :span="19">
+          <div class="grid-content bg-purple-light " @click="">{{'12345678'}}</div>
+        </el-col>
+      </el-row>
+    </div>
     <p style="margin: 1em"></p>
-    <el-row style="border-top:solid 1px #D5D5D5">
-      <el-col :span="5">
-        <div class="grid-content bg-purple ">{{'公司地址'}}</div>
-      </el-col>
-      <el-col :span="19">
-        <div class="grid-content bg-purple-light " @click="">{{'广州市天河区石牌桥'}}</div>
-      </el-col>
-    </el-row>
-    <el-row>
-      <el-col :span="5">
-        <div class="grid-content bg-purple ">{{'店铺/平台名称'}}</div>
-      </el-col>
-      <el-col :span="19">
-        <div class="grid-content bg-purple-light " @click="">{{'QWERTY'}}</div>
-      </el-col>
-    </el-row>
-    <el-row>
-      <el-col :span="5">
-        <div class="grid-content bg-purple ">{{'PC店铺/平台链接'}}</div>
-      </el-col>
-      <el-col :span="19">
-        <div class="grid-content bg-purple-light " @click="">{{'www.qwerty.com'}}</div>
-      </el-col>
-    </el-row>
-    <el-row>
-      <el-col :span="5">
-        <div class="grid-content bg-purple ">{{'手机店铺/平台链接'}}</div>
-      </el-col>
-      <el-col :span="19">
-        <div class="grid-content bg-purple-light " @click="">{{'m.qwerty.com'}}</div>
-      </el-col>
-    </el-row>
-    <el-row>
-      <el-col :span="5">
-        <div class="grid-content bg-purple ">{{'经营范围'}}</div>
-      </el-col>
-      <el-col :span="19">
-        <div class="grid-content bg-purple-light " @click="">{{'化妆品'}}</div>
-      </el-col>
-    </el-row>
-    <el-row>
-      <el-col :span="5">
-        <div class="grid-content bg-purple ">{{'经营过的类似商品'}}</div>
-      </el-col>
-      <el-col :span="19">
-        <div class="grid-content bg-purple-light " @click="">{{'LANCOM 保湿水润面膜'}}</div>
-      </el-col>
-    </el-row>
+    <div class="border1">
+      <el-row style="border-top:solid 1px #D5D5D5">
+        <el-col :span="5">
+          <div class="grid-content bg-purple ">{{'公司地址'}}</div>
+        </el-col>
+        <el-col :span="19">
+          <div class="grid-content bg-purple-light " @click="">{{'广州市天河区石牌桥'}}</div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="5">
+          <div class="grid-content bg-purple ">{{'店铺/平台名称'}}</div>
+        </el-col>
+        <el-col :span="19">
+          <div class="grid-content bg-purple-light " @click="">{{'QWERTY'}}</div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="5">
+          <div class="grid-content bg-purple ">{{'PC店铺/平台链接'}}</div>
+        </el-col>
+        <el-col :span="19">
+          <div class="grid-content bg-purple-light " @click="">{{'www.qwerty.com'}}</div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="5">
+          <div class="grid-content bg-purple ">{{'手机店铺/平台链接'}}</div>
+        </el-col>
+        <el-col :span="19">
+          <div class="grid-content bg-purple-light " @click="">{{'m.qwerty.com'}}</div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="5">
+          <div class="grid-content bg-purple ">{{'经营范围'}}</div>
+        </el-col>
+        <el-col :span="19">
+          <div class="grid-content bg-purple-light " @click="">{{'化妆品'}}</div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="5">
+          <div class="grid-content bg-purple ">{{'经营过的类似商品'}}</div>
+        </el-col>
+        <el-col :span="19">
+          <div class="grid-content bg-purple-light " @click="">{{'LANCOM 保湿水润面膜'}}</div>
+        </el-col>
+      </el-row>
+    </div>
     <!--合同签订below-->
     <el-row style="margin-top: 1.5em" class="border-top">
       <el-col :span="24">
