0f292271f45bedd96101e48ea43e26e0a87394e5
LFN <124547864@qq.com>
2018-11-30 13:18:29 +0800

    溯源生成-完成

--- src/views/goodManagement/sourceGeneration/index.vue
+++ src/views/goodManagement/sourceGeneration/index.vue
@@ -4,17 +4,45 @@
       <el-input @keyup.enter.native="handleFilter" style="width: 400px;" class="filter-item" placeholder="品牌序列号/品牌名称"
                 v-model="listQuery.keyword">
       </el-input>
-      <el-button class="filter-item" type="primary" v-waves icon="el-icon-search" @click="handleFilter">{{$t('table.search')}}</el-button>
+      <el-button class="filter-item" type="primary" v-waves icon="el-icon-search" @click="handleFilter">
+        {{$t('table.search')}}
+      </el-button>
     </div>
 
-     <el-table :data="list" v-loading="listLoading" element-loading-text="给我一点时间"
-              border fit highlight-current-row stripe size="mini" style="width: 100%" >
-      <el-table-column align="center" label="品牌序列号" prop="brandNo"/>
-      <el-table-column align="center" label="品牌名称（中文）" prop="chineseName" />
-      <el-table-column align="center" label="品牌名称（英文）" prop="englishName" />
-      <el-table-column align="center" label="原产国/产地" prop="provenance" />
-      <el-table-column align="center" label="品牌状态" prop="status" />
-      <el-table-column width= "200" align="center" :label="$t('order.operation')" class-name="small-padding">
+    <el-table
+      :data="list"
+      v-loading="listLoading" element-loading-text="给我一点时间"
+      border fit highlight-current-row stripe size="mini"
+      style="width: 100%"
+      class="border-top2 border-left2 border-right1">
+
+      <el-table-column align="center" label="品牌序列号"
+                       prop="brandNo"
+                       min-width="150"
+                       :filters="[{text: 'LANCOM', value: 123456},{text: 'AESOP', value: 654321}]"
+                       :filter-method="filterHandler" />
+
+      <el-table-column align="center" label="品牌名称（中文）"
+                       prop="chineseName"
+                       min-width="140"
+                       :filters="[{text: '兰蔻', value: '兰蔻'},{text: '伊索', value: '伊索'}]"
+                       :filter-method="filterHandler" />
+
+      <el-table-column align="center" label="品牌名称（英文）"
+                       prop="englishName"
+                       min-width="140"
+                       :filters="[{text: 'LANCOM', value: 'LANCOM'},{text: 'AESOP', value: 'AESOP'}]"
+                       :filter-method="filterHandler" />
+
+      <el-table-column align="center" label="原产国/产地" prop="provenance" min-width="140"/>
+
+      <el-table-column align="center" label="品牌状态"
+                       prop="status"
+                       min-width="110"
+                       :filters="[{text: '正常供货', value: '正常供货'},{text: '停止供货', value: '停止供货'}]"
+                       :filter-method="filterHandler" />
+
+      <el-table-column width="200" align="center" :label="$t('order.operation')" class-name="small-padding" fixed="right">
         <template slot-scope="scope">
           <el-button type="primary" size="mini" @click="productCodeDetail(scope.row)">查看商品码</el-button>
           <el-button type="primary" size="mini" @click="boxCodeDetail(scope.row)">查看箱码</el-button>
@@ -29,78 +57,82 @@
       </el-pagination>
     </div>
 
-    <el-dialog :visible.sync="isProductCodeDetailShow">
+    <el-dialog :visible.sync="isProductCodeDetailShow" title="查看商品码" width="80%">
       <product-code-detail :brand="currentBrand" v-if="isProductCodeDetailShow"></product-code-detail>
     </el-dialog>
 
-    <el-dialog :visible.sync="isBoxCodeDetailShow">
+    <el-dialog :visible.sync="isBoxCodeDetailShow" title="查看箱码" width="80%">
       <box-code-detail :brand="currentBrand" v-if="isBoxCodeDetailShow"></box-code-detail>
     </el-dialog>
   </div>
 </template>
 <script>
-import waves from '@/directive/waves'
-import productCodeDetail from './productCodeDetail'
-import boxCodeDetail from './boxCodeDetail'
+  import waves from '@/directive/waves'
+  import productCodeDetail from './productCodeDetail'
+  import boxCodeDetail from './boxCodeDetail'
 
-export default {
-  components: {
-    productCodeDetail,
-    boxCodeDetail
-  },
-  directives: {
-    waves
-  },
-  data() {
-    return {
-      listQuery: {
-        keyword: '',
-        page: 1,
-        rows: 20
+  export default {
+    components: {
+      productCodeDetail,
+      boxCodeDetail
+    },
+    directives: {
+      waves
+    },
+    data() {
+      return {
+        listQuery: {
+          keyword: '',
+          page: 1,
+          rows: 20
+        },
+        list: [],
+        isProductCodeDetailShow: false,
+        isBoxCodeDetailShow: false,
+        currentBrand: '',
+        listLoading: false,
+        total: 0
+      }
+    },
+    created() {
+      this.getList()
+    },
+    methods: {
+      getList() {
+        this.listLoading = true
+        this.list.push({
+          brandNo: 123456,
+          chineseName: '兰蔻',
+          englishName: 'LANCOME',
+          provenance: '法国',
+          status: '正常供货'
+        })
+        this.listLoading = false
       },
-      list: [],
-      isProductCodeDetailShow: false,
-      isBoxCodeDetailShow: false,
-      currentBrand: '',
-      listLoading: false,
-      total: 0
-    }
-  },
-  created() {
-    this.getList()
-  },
-  methods: {
-    getList() {
-      this.listLoading = true
-      this.list.push({
-        brandNo: '123456',
-        chineseName: '兰蔻',
-        englishName: 'Lancome',
-        provenance: '法国',
-        status: '正常供货'
-      })
-      this.listLoading = false
-    },
-    handleCurrentChange() {
-    },
-    productCodeDetail(row) {
-      this.isProductCodeDetailShow = true
-      this.currentBrand = row
-    },
-    boxCodeDetail(row) {
-      this.isBoxCodeDetailShow = true
-      this.currentBrand = row
-    },
-    handleFilter() {
-    },
-    handleSizeChange(val) {
-      this.listQuery.rows = val
+      handleCurrentChange() {
+      },
+      productCodeDetail(row) {
+        this.isProductCodeDetailShow = true
+        this.currentBrand = row
+      },
+      boxCodeDetail(row) {
+        this.isBoxCodeDetailShow = true
+        this.currentBrand = row
+      },
+      handleFilter() {
+      },
+      handleSizeChange(val) {
+        this.listQuery.rows = val
+      },
+      filterHandler(value, row, column) {
+        const property = column['property'];
+        return row[property] === value;
+      },
     }
   }
-}
 </script>
 <style>
-.el-table .cell {
-  word-break: break-word;
-}
+  .el-table .cell {
+    word-break: break-word;
+  }
 </style>
--- src/views/goodManagement/sourceGeneration/productCodeDetail.vue
+++ src/views/goodManagement/sourceGeneration/productCodeDetail.vue
@@ -1,101 +1,173 @@
 <template>
   <div>
-    <el-row :gutter="5">
-      <el-col :span="3" align="left"><div class="grid-content bg-purple">品牌序列号</div></el-col>
-      <el-col :span="21"><div class="grid-content bg-purple">{{brand.brandNo}}</div></el-col>
-    </el-row>
-    <el-row :gutter="5">
-      <el-col :span="3" align="left"><div class="grid-content bg-purple">{{$t('brand.brandName')}}</div></el-col>
-      <el-col :span="21"><div class="grid-content bg-purple">{{brand.englishName}}&nbsp;&nbsp;&nbsp;{{brand.chineseName}}</div></el-col>
-    </el-row>
+    <div class="border1" style="border-bottom-width: 2px">
+      <el-row>
+        <el-col :span="3" align="left">
+          <div class="grid-content bg-purple">品牌序列号</div>
+        </el-col>
+        <el-col :span="21">
+          <div class="grid-content bg-purple">{{brand.brandNo}}</div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="3" align="left">
+          <div class="grid-content bg-purple">{{$t('brand.brandName')}}</div>
+        </el-col>
+        <el-col :span="21">
+          <div class="grid-content bg-purple">{{brand.englishName}}&nbsp;&nbsp;&nbsp;{{brand.chineseName}}</div>
+        </el-col>
+      </el-row>
+    </div>
+    <p style="margin: 20px"></p>
 
     <div class="filter-container">
       <el-input @keyup.enter.native="handleFilter" style="width: 400px;" class="filter-item" placeholder="商品码/操作账号/接收邮箱"
                 v-model="listQuery.keyword" size="mini">
       </el-input>
-      <el-button class="filter-item" type="primary" v-waves icon="el-icon-search" @click="handleFilter" size="mini">{{$t('table.search')}}</el-button>
+      <el-button class="filter-item" type="primary" v-waves icon="el-icon-search" @click="handleFilter" size="mini">
+        {{$t('table.search')}}
+      </el-button>
     </div>
 
-    <el-row :gutter="5">
-      <el-col :span="21" align="left"><div class="grid-content bg-purple">已生成发送{{totalCount}}个商品码</div></el-col>
-    </el-row>
+    <p style="margin-top: 0">已生成发送&nbsp;{{totalCount}}&nbsp;个商品码</p>
 
     <!--清单-->
-    <el-table key='productList' :data="list" border fit highlight-current-row :summary-method="getSummaries" show-summary
-        v-loading="listLoading" element-loading-text="给我一点时间" stripe size="mini" style="width: 100%" >
-      <el-table-column align="center" label="商品码" prop="productCode" />
-      <el-table-column align="center" label="下载文件包名称" prop="downloadFile" />
-      <el-table-column align="center" label="操作账号" prop="createUserId" />
-      <el-table-column align="center" label="接收邮箱" prop="recipients" />
-      <el-table-column align="center" label="发送时间" prop="createTime" />
-
+    <el-table key='productList' :data="list"
+              border fit highlight-current-row size="mini"
+              :summary-method="getSummaries" show-summary
+              v-loading="listLoading" element-loading-text="给我一点时间"
+              class="border-top2 border-left2 border-right2"
+              style="width: 100%">
+      <el-table-column align="center" label="商品码" min-width="120px" prop="productCode"/>
+      <el-table-column align="center" label="下载文件包名称" min-width="150px" prop="">
+        <template slot-scope="scope">
+          <span>{{ downloaadFileBagName }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column align="center" label="操作账号" min-width="100px" prop="createUserId"/>
+      <el-table-column align="center" label="接收邮箱" min-width="130px" prop="recipients"/>
+      <el-table-column align="center" label="发送时间" min-width="130px" prop="createTime"/>
     </el-table>
   </div>
 </template>
 
 <script>
-import waves from '@/directive/waves'
+  import waves from '@/directive/waves'
+  import Mock from 'mockjs'
 
-export default {
-  name: 'productCodeDetail',
-  props: {
-    brand: {
-      type: Object,
-      default: () => {
-        return {}
+  export default {
+    name: 'productCodeDetail',
+    props: {
+      brand: {
+        type: Object,
+        default: () => {
+          return {}
+        }
+      }
+    },
+    directives: {
+      waves
+    },
+    computed: {
+      downloaadFileBagName() {
+        return this.brand.brandNo.toString() + '-' + this.totalCount.toString() + '-' + this.fileNameTime(new Date()).toString()
+      }
+    },
+    data() {
+      return {
+        list: [{
+          productCode: Mock.Random.natural(101, 199),
+          createUserId: Mock.Random.natural(1, 9),
+          recipients: Mock.Random.email('gmail.com'),
+          createTime: Mock.Random.now()
+        }],
+        listQuery: {
+          keyword: ''
+        },
+        totalCount: 0,
+        brandName: 'abcde',
+        codeAccumulate: '100',
+        listLoading: false,
+        resendLoading: false
+      }
+    },
+    created() {
+      this.getList()
+      this.totalCount = this.list.length
+    },
+    methods: {
+      getList() {
+        this.listLoading = true
+        this.listLoading = false
+      },
+      downloadFile(downloadFile) {
+        window.open(downloadFile)
+      },
+      resend(listObject) {
+        this.resendLoading = true
+        this.resendLoading = false
+      },
+      getSummaries(param) {
+        const sums = []
+        sums[0] = this.list.length
+        return sums
+      },
+      fileNameTime(date) {
+        const formatObj = {
+          y: date.getFullYear(),
+          m: date.getMonth() + 1,
+          d: date.getDate(),
+          h: date.getHours(),
+          i: date.getMinutes(),
+          s: date.getSeconds(),
+          a: date.getDay()
+        }
+        return formatObj.y.toString() + formatObj.m.toString() + formatObj.d.toString() + formatObj.h.toString() + formatObj.i.toString() + formatObj.s.toString()
       }
     }
-  },
-  directives: {
-    waves
-  },
-  computed: {
-  },
-  data() {
-    return {
-      list: [{
-        productCode: '123456',
-        downloadFile: 'download.zip',
-        createUserId: '1',
-        recipients: '0@mail.com',
-        createTime: '2018-09-01'
-      }],
-      listQuery: {
-        keyword: ''
-      },
-      totalCount: 0,
-      brandName: 'abcde',
-      codeAccumulate: '100',
-      listLoading: false,
-      resendLoading: false
-    }
-  },
-  created() {
-    this.getList()
-  },
-  methods: {
-    getList() {
-      this.listLoading = true
-      this.listLoading = false
-    },
-    downloadFile(downloadFile) {
-      window.open(downloadFile)
-    },
-    resend(listObject) {
-      this.resendLoading = true
-      this.resendLoading = false
-    },
-    getSummaries(param) {
-      const sums = []
-      sums[0] = this.list.length
-      return sums
-    }
   }
-}
 </script>
-<style>
-  .el-table .cell {
-    word-break: break-word;
+<style scoped>
+  .el-row {
+    border: 1px solid #D5D5D5;
+    border-bottom: none;
+  }
+
+  .el-col {
+    border-radius: 0;
+  }
+
+  .el-col:nth-child(odd) {
+    background: #dff2fc;
+    color: #424242;
+    font-weight: 700;
+    font-size: 12px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .el-col:nth-child(odd):not(:last-of-type) {
+    border-right: 1px solid #d5d5d5;
+  }
+
+  .el-col:nth-child(even) {
+    color: #424242;
+    font-size: 12px;
+    display: flex;
+    align-items: center;
+  }
+
+  .el-col:nth-child(even) div {
+    padding-left: 1em;
+  }
+
+  .el-col:nth-child(even):not(:last-of-type) {
+    border-right: 1px solid #d5d5d5;
+  }
+
+  .grid-content {
+    border: none;
   }
 </style>
 
