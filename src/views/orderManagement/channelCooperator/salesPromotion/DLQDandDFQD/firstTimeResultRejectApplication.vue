9c5815f27669052e4948cd6bbd8ee814a3402474
LFN <124547864@qq.com>
2018-12-07 23:33:28 +0800

    申请促销

---
+++ src/views/orderManagement/channelCooperator/salesPromotion/DLQDandDFQD/firstTimeResultRejectApplication.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="app-container">
+    <div class="theBorder">
+      <el-row>
+        <el-col :span="3" align="center">
+          <div class="grid-content bg-purple">渠道号</div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content bg-purple">{{goodsObject.channelNo}}</div>
+        </el-col>
+        <el-col :span="3" align="center">
+          <div class="grid-content bg-purple">渠道名称</div>
+        </el-col>
+        <el-col :span="6">
+          <div class="grid-content bg-purple">{{channelClassifyMap[goodsObject.channelClassify]}}</div>
+        </el-col>
+        <el-col :span="3" align="center">
+          <div class="grid-content bg-purple">渠道级别</div>
+        </el-col>
+        <el-col :span="3">
+          <div class="grid-content bg-purple">{{goodsObject.channelLevel}}</div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="3" align="center">
+          <div class="grid-content bg-purple">申请理由</div>
+        </el-col>
+        <el-col :span="21">
+          <div class="grid-content bg-purple">{{ applicatioiinReason }}</div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="3" align="center">
+          <div class="grid-content bg-purple">申请资料</div>
+        </el-col>
+        <el-col :span="10" style="height: 34px">
+          <div class="link-type fileName">111111.docx</div>
+          <div class="link-type fileName">1111111.pdf</div>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="3" align="center">
+          <div class="grid-content bg-purple">促销活动日期</div>
+        </el-col>
+        <el-col :span="12">
+          <div class="grid-content bg-purple">2019 年 07 月 07 日--2019 年 07 月 15 日</div>
+        </el-col>
+      </el-row>
+
+      <el-row>
+        <el-col :span="3">申请数量</el-col>
+        <el-col :span="2" class="no-border-right" style="display: flex;justify-content: center"><span>100</span></el-col>
+        <el-col :span="1" class="noBackground"><span>套</span></el-col>
+      </el-row>
+      <el-row>
+        <el-col :offset="3" :span="6"
+                style="background: #fff;font-weight: normal;border-bottom: none;justify-content: flex-start">
+          <div class="text-muted" style="background-color: #fff;font-weight: normal;color: #999">{{
+            $t('order.applicationTime') }}: {{ new Date()-1000000 | parseTime('{y}-{m}-{d} {h}:{i}:{s}') }}
+          </div>
+        </el-col>
+      </el-row>
+
+      <!--<el-row style="background-color: #D5D5D5;height: .5em"></el-row>-->
+      <el-row>
+        <el-col :span="3">审核结果</el-col>
+        <el-col :span="2" style="display: flex;justify-content: center"><span>驳回申请</span></el-col>
+      </el-row>
+      <el-row>
+        <el-col :offset="3" :span="6"
+                style="background: #fff;font-weight: normal;border-bottom: none;justify-content: flex-start">
+          <div class="text-muted" style="background-color: #fff;font-weight: normal;color: #999">{{
+            $t('order.reviewTime') }}: {{ new Date() | parseTime('{y}-{m}-{d} {h}:{i}:{s}') }}
+          </div>
+        </el-col>
+      </el-row>
+
+      <el-row class="no-border-bottom">
+        <!--<el-col :offset="3" :span="3" class="border-left">驳回说明</el-col>-->
+        <el-col :span="3">驳回说明</el-col>
+        <el-col :span="18" style="background: #fff;font-weight: normal;justify-content: flex-start;padding-left: 1em">
+          <div class="text-muted" style="background-color: #fff;font-weight: normal;color: #999">{{ rejectReason }}
+          </div>
+        </el-col>
+      </el-row>
+      <!--<el-row style="background-color: #D5D5D5;height: .5em"></el-row>-->
+    </div>
+  </div>
+</template>
+
+<script>
+  import waves from '@/directive/waves' // 水波纹指令
+
+  export default {
+    name: 'applicationForm',
+    directives: {
+      waves
+    },
+    props: {
+      goodsObject: {
+        type: Object,
+        default: () => {
+          return {}
+        }
+      },
+      operationIndex: {
+        type: Number,
+        required: true
+      }
+    },
+    data() {
+      return {
+        list: [],
+        dateRange: [],
+        fileList: [],
+        failDialogVisible: false,
+        successDialogVisible: false,
+        submitLoading: false,
+        channelClassifyMap: {
+          1: '独立渠道',
+          2: '代发渠道',
+          3: '代发渠道'
+        },
+        applicatioiinReason: '双十一活动。',
+        rejectReason: '促销时间太长。'
+      }
+    },
+    methods: {}
+  }
+</script>
+
+<style scope>
+  .el-input--medium .el-input__inner {
+    line-height: 24px;
+    height: 24px;
+  }
+</style>
---
+++ src/views/orderManagement/channelCooperator/salesPromotion/FXQD/firstTimeApplicationForm.vue
@@ -0,0 +1,214 @@
+<template>
+  <div class="app-container">
+
+    <div class="theBorder">
+      <el-row>
+        <el-col :span="3" align="center">
+          <div class="grid-content bg-purple">分销渠道号</div>
+        </el-col>
+        <el-col :span="9">
+          <div class="grid-content bg-purple">{{goodsObject.channelNo}}</div>
+        </el-col>
+        <el-col :span="3" align="center">
+          <div class="grid-content bg-purple">分销渠道名称</div>
+        </el-col>
+        <el-col :span="9">
+          <div class="grid-content bg-purple">{{channelClassifyMap[goodsObject.channelClassify]}}</div>
+        </el-col>
+      </el-row>
+      <el-row type="flex" class="searchRow" style="border: none;margin: 1em 0">
+        <div style="width: 80%; padding-left: 1em">
+          <el-input placeholder="子渠道号/子渠道名称"></el-input>
+        </div>
+        <div style="width: 20%;margin-left: 1em">
+          <el-button type="primary">搜索子渠道</el-button>
+        </div>
+      </el-row>
+
+      <div v-for="(item, index) in goodsObject.FXMSG" class="border-top">
+        <el-row style="height: 2em;background-color: #d5d5d5"></el-row>
+        <el-row>
+          <el-col :span="3" align="center">
+            <div class="grid-content bg-purple">子渠道号</div>
+          </el-col>
+          <el-col :span="6">
+            <div class="grid-content bg-purple">{{item.channelNo}}</div>
+          </el-col>
+          <el-col :span="3" align="center">
+            <div class="grid-content bg-purple">子渠道名称</div>
+          </el-col>
+          <el-col :span="6">
+            <div class="grid-content bg-purple">{{channelClassifyMap[item.channelClassify]}}</div>
+          </el-col>
+          <el-col :span="3" align="center">
+            <div class="grid-content bg-purple">子渠道级别</div>
+          </el-col>
+          <el-col :span="3">
+            <div class="grid-content bg-purple">{{item.channelLevel}}</div>
+          </el-col>
+        </el-row>
+
+        <el-row>
+          <el-col :span="3" align="center">
+            <div class="grid-content bg-purple">申请理由</div>
+          </el-col>
+          <el-col :span="21">
+            <el-input type="textarea" :rows="1" placeholder="100字以内简要说明理由" class="noBorderTextarea"></el-input>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="3" align="center" style="min-height: 64px;">
+            <div class="grid-content bg-purple">上传申请资料</div>
+          </el-col>
+          <el-col :span="10" style="padding-left: 1em">
+            <el-upload class="upload-demo" ref="upload" action="https://jsonplaceholder.typicode.com/posts/"
+                       :on-preview="handlePreview" :on-remove="handleRemove" :file-list="fileList" :auto-upload="false">
+              <div slot="tip" class="el-upload__tip">提示：需上传活动方案、促销版面申请成功截图</div>
+              <el-button slot="trigger" size="mini" v-waves type="primary" style="margin-top: 5px">浏览</el-button>
+            </el-upload>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="3" align="center" style="min-height: 36px">
+            <div class="grid-content bg-purple">促销活动日期</div>
+          </el-col>
+          <el-col :span="8">
+            <el-date-picker class="noBorderDatePicker" v-model="dateRange" type="daterange"
+                            range-separator="————" start-placeholder="开始日期"
+                            end-placeholder="结束日期"></el-date-picker>
+          </el-col>
+          <el-col :span="11" align="center" style="background-color: transparent">
+            <div align="left" style="color:red;line-height: 36px">温馨提示：促销活动的开始日期距离今天需要≥35天。</div>
+          </el-col>
+        </el-row>
+
+        <el-row>
+          <el-col :span="3">申请数量</el-col>
+          <el-col :span="3" style="border-right: none">
+            <el-input class="noBorderInput" placeholder="输入申请数量"></el-input>
+          </el-col>
+          <el-col :span="1" class="noBackground"><span>套</span></el-col>
+          <el-col :span="0"></el-col>
+          <el-col :span="3">库存数量</el-col>
+          <el-col :span="1" style="display: flex;justify-content: center;border-right: none"><span>1000</span></el-col>
+          <el-col :span="1" class="noBackground"><span>套</span></el-col>
+        </el-row>
+        <el-row class="no-border-bottom">
+          <el-col :offset="8" :span="10" class="noBackground"><span style="color: grey">“库存数量”仅供参考，以吾蜜公司同意申请后才正式为该申请预留库存。</span></el-col>
+        </el-row>
+      </div>
+    </div>
+
+
+    <div class="dialogBottomButton-wrap">
+      <el-button type="primary" @click="submitApplication" :loading="submitLoading">提交申请</el-button>
+    </div>
+
+    <el-dialog :visible.sync="failDialogVisible" width="30%" append-to-body :show-close="false">
+      <span>提交申请不成功，部分子渠道的促销活动开始日期距离今天不足35天，请检查。</span>
+      <div slot="footer" class="dialog-footer" style="text-align: center">
+        <el-button type="primary" @click="failDialogVisible = false, submitLoading = false">知道了</el-button>
+      </div>
+    </el-dialog>
+
+    <el-dialog :visible.sync="successDialogVisible" width="30%" append-to-body :show-close="false">
+      <span>申请已提交，可点击“首次结果”了解进度和详情。</span>
+      <div slot="footer" class="dialog-footer" style="text-align: center">
+        <el-button type="primary" @click="handleConfirm()">知道了</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+  import waves from '@/directive/waves' // 水波纹指令
+
+  export default {
+    name: 'applicationForm',
+    directives: {
+      waves
+    },
+    props: {
+      goodsObject: {
+        type: Object,
+        default: () => {
+          return {}
+        }
+      },
+      operationIndex: {
+        type: Number,
+        required: true
+      }
+    },
+    data() {
+      return {
+        list: [],
+        dateRange: [
+          [], [], []
+        ],
+        fileList: [],
+        failDialogVisible: false,
+        successDialogVisible: false,
+        submitLoading: false,
+        channelClassifyMap: {
+          1: '独立渠道',
+          2: '代发渠道',
+          3: '代发渠道'
+        }
+      }
+    },
+    methods: {
+      submitApplication() {
+        this.submitLoading = true
+        this.dateRange.forEach((item, index) => {
+          if (item === []) {
+            this.failDialogVisible = true
+            return
+          }
+        })
+//        if (this.dateRange === '' || this.dateRange.length === 0) {
+//          this.failDialogVisible = true
+//          return
+//        }
+        const now = Date.now()
+        const diff1 = parseInt(this.dateRange[0][0] - now) / (1000 * 60 * 60 * 24) + 1
+        const diff2 = parseInt(this.dateRange[1][0] - now) / (1000 * 60 * 60 * 24) + 1
+        const diff3 = parseInt(this.dateRange[2][0] - now) / (1000 * 60 * 60 * 24) + 1
+        if (diff1 >= 35 && diff2 >= 35 && diff3 >= 35) {
+          this.successDialogVisible = true
+          this.$emit('submit-first-application')
+        } else {
+          this.failDialogVisible = true
+        }
+//        const now = Date.now()
+//        const diff = parseInt(this.dateRange[0] - now) / (1000 * 60 * 60 * 24) + 1
+//        if (diff >= 35) {
+//          this.successDialogVisible = true
+//          this.$emit('submit-first-application')
+//        } else {
+//          this.failDialogVisible = true
+//        }
+      },
+      handlePreview() {
+      },
+      handleRemove() {
+      },
+      handleConfirm() {
+        this.$emit('closeDLDFfirstApplicationForm')
+        this.submitLoading = false
+      }
+    }
+  }
+</script>
+
+<style scope>
+  .searchRow .el-input--medium .el-input__inner {
+    line-height: 36px;
+    height: 36px;
+  }
+
+  .el-input--medium .el-input__inner {
+    line-height: 24px;
+    height: 24px;
+  }
+</style>
